43bc60477a5202ef6d971da86e0502a3051c73ab
binhdev <ttbinh@hueic.edu.vn>
2017-08-24 11:29:33 +0700

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
   def full_title page_title = ""
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if base_title.empty?
-      base_title
+    if page_title.empty?
+      I18n.t("base_title")
     else
-      page_title + " | " + base_title
+      page_title + " | " + I18n.t("base_title")
     end
   end
 end
